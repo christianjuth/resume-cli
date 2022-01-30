14728de3be7781ab97621a5e7c9b139e68b66d86
Christian Juth <cjuth2@gmail.com>
2022-01-30 14:00:30 -0500

    fix: links

--- .cli/index.js
+++ .cli/index.js
@@ -6,48 +6,45 @@ var openurl_1 = require("openurl");
  * Follow me on Twitter 🐦
  */
 function twitter() {
-    (0, openurl_1.open)("<your Twitter url>");
+    (0, openurl_1.open)("https://twitter.com/christianjuth");
 }
 /**
  * Connect with my on LinkedIn 👔
  */
 function linkedin() {
-    (0, openurl_1.open)("<your LinkedIn url>");
+    (0, openurl_1.open)("https://www.linkedin.com/in/christian--j/");
 }
 /**
  * See my projects on GitHub 🚀
  */
 function github() {
-    (0, openurl_1.open)("<your GitHub url>");
+    (0, openurl_1.open)("https://github.com/christianjuth");
 }
 /**
  * Check out some of my projects ✨
  */
 function projects(str) {
     switch (str) {
-        case "project1":
-            (0, openurl_1.open)("https://...");
+        case "tic-tac-toe-ai-in-rust":
+            (0, openurl_1.open)("https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=2331928209d05d2bf9d03457cfb8b0c9");
             break;
-        case "project2":
-            (0, openurl_1.open)("https://...");
+        case "dfs-sudoku-solver":
+            (0, openurl_1.open)("https://npm.christianjuth.com/sudoku-solver");
+        case "react-photo-editor":
+            (0, openurl_1.open)("https://laughing-pare-d218eb.netlify.app/");
+        case "graph-search-visualizer":
+            (0, openurl_1.open)("https://graph-search-visualizer.vercel.app/");
+        case "minimax-pruning-solver":
+            (0, openurl_1.open)("https://minimax-tree-pruning.vercel.app/");
+        case "spotify-random-playlist-generator":
+            (0, openurl_1.open)("https://spotify-random-playlist-generator.vercel.app/");
+        case "react-state-machine-library":
+            (0, openurl_1.open)("https://react-state-machines.vercel.app/");
     }
-}
-/**
- * Contact me 📪
- */
-function contact(info) {
-    var subject = info.subject, name = info.name, email = info.email, phoneNumber = info.phoneNumber;
-    var EMAIL = "your.email@example.com";
-    var message = "\n    Name: ".concat(name, "\n    Email: ").concat(email, "\n    ").concat(phoneNumber ? "Number: ".concat(phoneNumber) : "", "\n  ");
-    (0, openurl_1.mailto)([EMAIL], {
-        subject: subject,
-        body: message
-    });
 }
 exports.cli = {
     twitter: twitter,
     linkedin: linkedin,
     github: github,
-    projects: projects,
-    contact: contact
+    projects: projects
 };
--- index.ts
+++ index.ts
@@ -4,58 +4,55 @@ import { open, mailto } from "openurl";
  * Follow me on Twitter 🐦
  */
 function twitter() {
-  open("<your Twitter url>");
+  open("https://twitter.com/christianjuth");
 }
 
 /**
  * Connect with my on LinkedIn 👔
  */
 function linkedin() {
-  open("<your LinkedIn url>");
+  open("https://www.linkedin.com/in/christian--j/");
 }
 
 /**
  * See my projects on GitHub 🚀
  */
 function github() {
-  open("<your GitHub url>");
+  open("https://github.com/christianjuth");
 }
 
 /**
  * Check out some of my projects ✨
  */
-function projects(str: "project1" | "project2") {
+function projects(
+  str:
+    | "tic-tac-toe-ai-in-rust"
+    | "dfs-sudoku-solver"
+    | "react-photo-editor"
+    | "graph-search-visualizer"
+    | "minimax-pruning-solver"
+    | "spotify-random-playlist-generator"
+    | "react-state-machine-library"
+) {
   switch (str) {
-    case "project1":
-      open("https://...");
+    case "tic-tac-toe-ai-in-rust":
+      open(
+        "https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=2331928209d05d2bf9d03457cfb8b0c9"
+      );
       break;
-    case "project2":
-      open("https://...");
+    case "dfs-sudoku-solver":
+      open("https://npm.christianjuth.com/sudoku-solver");
+    case "react-photo-editor":
+      open("https://laughing-pare-d218eb.netlify.app/");
+    case "graph-search-visualizer":
+      open("https://graph-search-visualizer.vercel.app/");
+    case "minimax-pruning-solver":
+      open("https://minimax-tree-pruning.vercel.app/");
+    case "spotify-random-playlist-generator":
+      open("https://spotify-random-playlist-generator.vercel.app/");
+    case "react-state-machine-library":
+      open("https://react-state-machines.vercel.app/");
   }
-}
-
-/**
- * Contact me 📪
- */
-function contact(info: {
-  subject: string;
-  name: string;
-  email: string;
-  phoneNumber: string;
-}) {
-  const { subject, name, email, phoneNumber } = info;
-  const EMAIL = "your.email@example.com";
-
-  const message = `
-    Name: ${name}
-    Email: ${email}
-    ${phoneNumber ? `Number: ${phoneNumber}` : ""}
-  `;
-
-  mailto([EMAIL], {
-    subject,
-    body: message,
-  });
 }
 
 export const cli = {
@@ -63,5 +60,4 @@ export const cli = {
   linkedin,
   github,
   projects,
-  contact,
 };
